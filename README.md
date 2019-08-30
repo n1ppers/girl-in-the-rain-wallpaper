22d7f670fea5285846edba173a4e51b4fab59914
OwO#8196 <specialforgames251@gmail.com>
2019-08-30 09:30:36 +0300

    2019.08.30 update
    Added WIP features:
    - Smooth
    - Channel reverse

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Web-based wallpaper for Wallpaper Engine
 
 ### Steam Workshop: https://steamcommunity.com/sharedfiles/filedetails/?id=1676874831
+### Preview: https://www.youtube.com/watch?v=enWsyueR_Vo
--- girl-in-the-rain/vis.js
+++ girl-in-the-rain/vis.js
@@ -1,5 +1,5 @@
-var canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 canvas.id = "visualizer";
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -10,7 +10,11 @@ var settings = {
     viscolor: "165, 165, 235",
     viscolora: 215,
     barh: 300,
-    barpadding: 4
+    barpadding: 4,
+    wipfeatures: false,
+    smooth: false,
+    sfactor: 5,
+    reverse: 0
 };
 
 window.wallpaperPropertyListener = {
@@ -37,6 +41,22 @@ window.wallpaperPropertyListener = {
             settings.barpadding = properties.barpadding.value;
             update_size();
         }
+        if (properties.wipfeatures) {
+            settings.wipfeatures = properties.wipfeatures.value;
+            update_data();
+        }
+        if (properties.smooth) {
+            settings.smooth = properties.smooth.value;
+            update_data();
+        }
+        if (properties.sfactor) {
+            settings.sfactor = properties.sfactor.value;
+            update_data();
+        }
+        if (properties.reverse) {
+            settings.reverse = properties.reverse.value;
+            update_data();
+        }
     }
 };
 
@@ -44,6 +64,8 @@ var audio = [];
 var listener = function(arr) {
     audio = arr
 };
+
+var data = [256];
 
 var bars = {
     padding: settings.barpadding,
@@ -54,24 +76,23 @@ var bars = {
 
 function draw() {
     if (settings.vis) {
+        update_data();
         context.fillStyle = bars.color;
         context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        for (var [i, part] of audio.entries()) {
+        var counter = 0;
+        for (var [i, part] of data.entries()) {
             var x = (i * bars.widht) + ((i + 1) * bars.padding) - (bars.padding / 2);
-            var y = 525;
-            var h = bars.height;
-            var pos = h;
-            var smooth = 25; //to-do
-            if(h < pos) {
-                h+=(pos-h)/smooth;
-            } else if(h > pos) {
-                h-=(h-pos)/smooth;
-            }
-            context.fillRect(x, y, bars.widht, h * -part);
+            //var y = 525;
+            var y = window.innerHeight / 2.0571429;
+            var w = bars.widht;
+            var h = settings.smooth ? (bars.height * -part) * 2 : bars.height * -part;
+            context.fillRect(x, y, w, h);
         }
+        //document.getElementById("d").innerHTML = i;
     }
     else {
         context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        data = [];
     }
     requestAnimationFrame(draw);
 }
@@ -84,6 +105,45 @@ function update_size() {
 
 function update_color() {
     bars.color = settings.viscolor;
+}
+
+function update_data() {
+    data = [];
+    if (!settings.wipfeatures) {
+        for (var i = 0; i <= audio.length; i++) {
+            data[i] = audio[i];
+        }
+    }
+    else {
+        for (var i = 0; i <= audio.length; i++) {
+            data[i] = settings.smooth ? (audio[i - 1] * 1 + audio[i] * 2 + audio[i + 1] * 1) / settings.sfactor : audio[i];
+        }
+        
+        if (settings.reverse != 0) {
+            if (settings.reverse == 1) {
+                data = reverseRight();
+            }
+            else if (settings.reverse == 2) {
+    
+            }
+        }
+    }
+}
+
+function reverseRight() {
+    var newArray = [];
+    for (var j = 0; j <= 64; j++) {
+        newArray[j] = settings.smooth ? (audio[j - 1] * 1 + audio[j] * 2 + audio[j + 1] * 1) / settings.sfactor : audio[j];
+    }
+    for (var i = audio.length - 1; i >= 64; i--) {
+        settings.smooth ? newArray.push((audio[i - 1] * 1 + audio[i] * 2 + audio[i + 1] * 1) / settings.sfactor) : newArray.push(audio[i]);
+    }
+    return newArray.slice(1);
+}
+
+function reverseLeft() {
+    var newArray = [];
+    return newArray;
 }
 
 window.onload = function() {
